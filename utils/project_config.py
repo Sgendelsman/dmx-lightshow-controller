076a2178cf65c8cf9953c254d0e889fb60f69429
sgendelsman <sgendelsman@gmail.com>
2025-07-27 13:31:13 -0400

    Added music

--- utils/project_config.py
+++ utils/project_config.py
@@ -13,12 +13,12 @@ START_DELAY = 5 # How many seconds to give all of the processes to sync up befor
 # seek -> How many seconds into the song should the audio playback begin. Helpful for testing
 # beat_delay -> How many seconds to push back beats from the audio. Helps them sync up (both testing AND live)
 SONGS = [
-    # {'song': f'{MUSIC_DIRECTORY}/unbroken.mp3', 'start_early': 0, 'seek': 0.0, 'beat_delay': 0.180},
-    # {'song': f'{MUSIC_DIRECTORY}/kairo.mp3', 'start_early': 6.495, 'seek': 0.0, 'beat_delay': 0.120},
-    # {'song': f'{MUSIC_DIRECTORY}/doruksen_song1.mp3', 'start_early': 0.774, 'seek': 0.0, 'beat_delay': -0.085},
-    {'song': f'{MUSIC_DIRECTORY}/doruksen_song2_1.mp3', 'start_early': 0.59, 'seek': 94.0, 'beat_delay': 0.046},
-    {'song': f'{MUSIC_DIRECTORY}/kissmealready.mp3', 'start_early': 76.00, 'seek': 0.0, 'beat_delay': 0},
-    # {'song': f'{MUSIC_DIRECTORY}/doruksen_song2_2.mp3', 'start_early': 1.736, 'seek': 0.0, 'beat_delay': -0.060}
+    {'song': f'{MUSIC_DIRECTORY}/unbroken.mp3', 'start_early': 0, 'seek': 0.0, 'beat_delay': 0.520},#0.180},
+    {'song': f'{MUSIC_DIRECTORY}/kairo.mp3', 'start_early': 6.495, 'seek': 0.0, 'beat_delay': 0.460},#0.120},
+    {'song': f'{MUSIC_DIRECTORY}/doruksen_song1.mp3', 'start_early': 0.774, 'seek': 0.0, 'beat_delay': 0.255},#-0.085},
+    {'song': f'{MUSIC_DIRECTORY}/doruksen_song2_1.mp3', 'start_early': 0.59, 'seek': 0.0, 'beat_delay': 0.386},#0.046},
+    {'song': f'{MUSIC_DIRECTORY}/kissmealready.mp3', 'start_early': 76.00, 'seek': 0.0, 'beat_delay': 0.340},#0},
+    {'song': f'{MUSIC_DIRECTORY}/doruksen_song2_2.mp3', 'start_early': 1.736, 'seek': 0.0, 'beat_delay': 0.280}#-0.060}
 ]
 
 MAX_BRIGHTNESS = 100 # 0 - 100%
